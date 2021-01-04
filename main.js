daec7e317d2efb5a6384940745d80ef41c83746b
natchez <natchezzz@hotmail.com>
2021-01-04 14:33:33 +0100

    fin

---
+++ main.js
@@ -0,0 +1,88 @@
+                // EXO 1
+
+// let classe = ['Alex','Max','Ayhan','Natchez']
+// console.log(classe)
+// console.log(classe[3])
+// console.log(classe.length)
+
+                // EXO 2
+
+// classe.push('Zakaria')
+// console.log(classe)
+
+                // EXO 3
+
+
+let classeCoding16 = []
+// console.log(classeCoding16)
+// 8h43
+alert('8h43')
+classeCoding16.push('Ayhan')
+// console.log(classeCoding16)
+console.log(`${classeCoding16} arrive en classe`);
+// 8h44
+alert('8h44')
+classeCoding16.push('Benedicte')
+// console.log(classeCoding16)
+console.log(`${classeCoding16[1]} rentre, allume son PC et commence à mettre à jour ses notes`);
+// 8h45
+alert('8h45')
+classeCoding16.push('Louise','Jamila')
+// console.log(classeCoding16)
+console.log(`${classeCoding16[2]} et ${classeCoding16[3]} rejoignent la classe`);
+// 8h51
+alert('8h51')
+classeCoding16.push('Sebastien','Adame')
+// console.log(classeCoding16)
+console.log(`${classeCoding16[4]} et ${classeCoding16[5]} voyent Nicolas passer devant la porte, et décident de le rejoindre`);
+// 8h55
+alert('8h55')
+classeCoding16.push('Dawid','Malo')
+// console.log(classeCoding16)
+console.log(`${classeCoding16[6]} et ${classeCoding16[7]}  rentrent en classe en faisant un maximum de bruit`);
+// 8h56
+alert('8h56')
+classeCoding16.push('Alex')
+// console.log(classeCoding16)
+console.log(`${classeCoding16[8]}  rentre en classe puis redescend prendre sa tasse de café`);
+// 8h59
+alert('8h59')
+classeCoding16.push('Maxence','Mohammed')
+// console.log(classeCoding16)
+console.log(`${classeCoding16[9]} et ${classeCoding16[10]} rentre en classe saluant tout le monde`);
+// 9h00
+alert('9h')
+classeCoding16.push('Saliou')
+// console.log(classeCoding16)
+console.log(`${classeCoding16[11]} rentre tranquil et observe tout le monde`);
+// 9h03
+alert('9h03')
+classeCoding16.push('Zulma ','Maximilien')
+// console.log(classeCoding16)
+console.log(`${classeCoding16[12]} et ${classeCoding16[13]} rentrent et observent ${classeCoding16[3]} qui à l'air d'être tranquilleeee`);
+// 9h04
+alert('9h04')
+classeCoding16.push('Natchez')
+// console.log(classeCoding16)
+console.log(`${classeCoding16[14]} rentre en classe avec sa tasse de café`);
+// 9h05
+alert('9h05')
+classeCoding16.push('Zakaria','Ayoub')
+// console.log(classeCoding16)
+console.log(`${classeCoding16[15]} et ${classeCoding16[16]} rentrent en classe tous contents de n'être que 5 minutes en retard, ${classeCoding16[0]} leur dit qu'ils sont en retard et puis il sort`);
+// 9h10
+alert('9h10')
+classeCoding16.push('Achraf')
+// console.log(classeCoding16)
+console.log(`${classeCoding16[17]} rentre avec sa petite tasse de café et son pc, tout content et souriant`);
+// 9h20
+alert('9h20')
+// console.log(classeCoding16)
+console.log(`${classeCoding16[0]} commencent a donner cours`);
+// 9h30
+alert('9h30')
+classeCoding16.push('Abdellah')
+// console.log(classeCoding16)
+console.log(`${classeCoding16[18]} rentre en faisant le minimum de bruit possible mais toute la classe la regarde`);
+
+
